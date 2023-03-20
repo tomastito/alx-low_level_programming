a08f8fc29c12f41edf59d3526d912329ecaa1b88
tomas <tomasawad18@gmil.com>
2023-03-20 07:37:22 -0700

    defult

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,14 @@
  * main -Entry point
  *
  * Description: A C program that prints with printf function.
+ *
  * Return: Always 1 (not success)
 */
 
 int main(void)
 {
-	char que[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(1,que,59);
+	write(1,quo,59);
 	return (1);
 }
