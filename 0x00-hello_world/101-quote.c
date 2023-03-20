36dd011c1d1debf9f68503f878092f9e28755330
tomas <tomasawad18@gmil.com>
2023-03-20 07:42:14 -0700

    defult

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,11 @@
  *
  * Description: A C program that prints with printf function.
  *
- * Return: Always 1 (not success)
+ * Return: 1 (error).
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1,quo,59);
 	return (1);
