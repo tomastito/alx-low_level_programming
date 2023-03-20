6873a3c5b8c0f8667300b617e126a96031824c0e
tomas <tomasawad18@gmil.com>
2023-03-20 06:46:33 -0700

    defult

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- *main - entry point
+ * main - Entry point
  *
- * description: AC program that prints with put function
+ * description: A C program that prints with put function
  *
- *Return: Alawys 0(success)
+ * Return: Alawys 0(success)
  */
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
